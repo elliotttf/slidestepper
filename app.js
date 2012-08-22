73776e3179f996221389310d55e7550e5d8d59b3
Elliott Foster <elliottf@codebrews.com>
2012-08-22 17:14:51 -0500

    Functional socket controller.

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-var pass = 'unasfe'; // TODO
+var flatiron = require('flatiron');
+var path = require('path');
+var app = flatiron.app;
+
+app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
+
 var io = require('socket.io').listen(3000);
 
 io.sockets.on('connection', function(socket) {
   socket.emit('connected');
-  console.log('yo dawg!');
 
   socket.on('authenticate', function(auth) {
-    if (auth === pass) {
+    if (auth === app.config.get('pass')) {
       socket.emit('authenticated', ':)');
 
       socket.on('navigateTo', function(command) {
